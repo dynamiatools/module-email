0cc9d14929210500ad33aa540522b4653e02c565
Mario Serrano <mario@dynamiasoluciones.com>
2019-05-26 06:43:29 -0500

    licensed to lgpl v3

--- sources/core/src/main/java/tools/dynamia/modules/email/EmailMessage.java
+++ sources/core/src/main/java/tools/dynamia/modules/email/EmailMessage.java
@@ -1,6 +1,28 @@
 
 package tools.dynamia.modules.email;
 
+/*-
+ * #%L
+ * DynamiaModules - Email
+ * %%
+ * Copyright (C) 2018 - 2019 Dynamia Soluciones IT SAS
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
 import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -18,6 +40,7 @@ import tools.dynamia.modules.email.domain.EmailTemplate;
 import tools.dynamia.modules.email.services.EmailService;
 
 /**
+ * Email message helper. Use this class to create a setup an email message
  * @author Mario Serrano Leones
  */
 public class EmailMessage implements Serializable {
