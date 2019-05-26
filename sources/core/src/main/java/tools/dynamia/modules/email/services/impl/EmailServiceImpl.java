1a164adc5b8b36377f3520e9b890cf90a09b74c1
Mario Serrano <mario@dynamiasoluciones.com>
2019-05-26 06:14:47 -0500

    update dependencies

--- sources/core/src/main/java/tools/dynamia/modules/email/services/impl/EmailServiceImpl.java
+++ sources/core/src/main/java/tools/dynamia/modules/email/services/impl/EmailServiceImpl.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tools.dynamia.modules.email.services.impl;
 
 import java.io.StringWriter;
