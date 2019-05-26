9a92d2bbe587f936d91c2d9362f2c30664e93a90
Mario Serrano <mario@dynamiasoluciones.com>
2019-05-26 06:42:21 -0500

    Future support

--- sources/core/src/main/java/tools/dynamia/modules/email/services/EmailService.java
+++ sources/core/src/main/java/tools/dynamia/modules/email/services/EmailService.java
@@ -1,26 +1,87 @@
 
 package tools.dynamia.modules.email.services;
 
+/*-
+ * #%L
+ * DynamiaModules - Email
+ * %%
+ * Copyright (C) 2018 - 2019 Dynamia Soluciones IT SAS
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
 import tools.dynamia.modules.email.EmailMessage;
+import tools.dynamia.modules.email.EmailSendResult;
 import tools.dynamia.modules.email.domain.EmailAccount;
 import tools.dynamia.modules.email.domain.EmailTemplate;
 
+import java.util.concurrent.Future;
+
 /**
+ * Email service for sending emails
  *
- * @author ronald
+ * @author Mario Serrano Leones
  */
 public interface EmailService {
 
-	public void send(EmailMessage message);
+    /**
+     * Send email message asynchronously
+     *
+     * @param message
+     */
+    Future<EmailSendResult> send(EmailMessage message);
 
-	public void send(String to, String subject, String content);
+    /**
+     * Build and send email message asynchronously
+     *
+     * @param to
+     * @param subject
+     * @param content
+     */
+    Future<EmailSendResult> send(String to, String subject, String content);
 
-	public void setPreferredEmailAccount(EmailAccount account);
+    /**
+     * Setup preferred email account in current SaaS account
+     *
+     * @param account
+     */
+    void setPreferredEmailAccount(EmailAccount account);
 
-	public EmailAccount getPreferredEmailAccount();
+    /**
+     * Get preferred email account in current SaaS account
+     *
+     * @return
+     */
+    EmailAccount getPreferredEmailAccount();
 
-	EmailTemplate getTemplateByName(String name, boolean autocreate);
+    /**
+     * Find email template by name in current SaaS account. If autocreate is true a new blank email template is created
+     *
+     * @param name
+     * @param autocreate
+     * @return
+     */
+    EmailTemplate getTemplateByName(String name, boolean autocreate);
 
-	public EmailTemplate getTemplateByName(String name);
+    /**
+     * Find email template by name
+     *
+     * @param name
+     * @return
+     */
+    EmailTemplate getTemplateByName(String name);
 
 }
