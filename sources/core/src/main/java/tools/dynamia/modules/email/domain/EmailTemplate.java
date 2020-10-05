9cd900f0794e30a609d79d303ea4671adcd0ed1e
Mario Serrano <mario@dynamiasoluciones.com>
2020-10-05 11:28:17 -0500

    update license text

--- sources/core/src/main/java/tools/dynamia/modules/email/domain/EmailTemplate.java
+++ sources/core/src/main/java/tools/dynamia/modules/email/domain/EmailTemplate.java
@@ -1,26 +1,21 @@
-package tools.dynamia.modules.email.domain;
-
-/*-
- * #%L
- * DynamiaModules - Email
- * %%
- * Copyright (C) 2018 - 2019 Dynamia Soluciones IT SAS
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
+/*
+ * Copyright (C)  2020. Dynamia Soluciones IT S.A.S - NIT 900302344-1
+ * Colombia - South America
+ *  All Rights Reserved.
+ *
+ * This file is free software: you can redistribute it and/or modify it  under the terms of the
+ *  GNU Lesser General Public License (LGPL v3) as published by the Free Software Foundation,
+ *   either version 3 of the License, or (at your option) any later version.
+ *
+ *  This file is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU Lesser General Public License for more details. You should have received a copy of the
+ *   GNU Lesser General Public License along with this file.
+ *   If not, see <https://www.gnu.org/licenses/>.
+ *
  */
+
+package tools.dynamia.modules.email.domain;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
--- sources/core/src/main/java/tools/dynamia/modules/email/services/EmailService.java
+++ sources/core/src/main/java/tools/dynamia/modules/email/services/EmailService.java
@@ -1,27 +1,22 @@
 
-package tools.dynamia.modules.email.services;
-
-/*-
- * #%L
- * DynamiaModules - Email
- * %%
- * Copyright (C) 2018 - 2019 Dynamia Soluciones IT SAS
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+/*
+ * Copyright (C)  2020. Dynamia Soluciones IT S.A.S - NIT 900302344-1
+ * Colombia - South America
+ *  All Rights Reserved.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
+ * This file is free software: you can redistribute it and/or modify it  under the terms of the
+ *  GNU Lesser General Public License (LGPL v3) as published by the Free Software Foundation,
+ *   either version 3 of the License, or (at your option) any later version.
  *
- * You should have received a copy of the GNU General Lesser Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
+ *  This file is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU Lesser General Public License for more details. You should have received a copy of the
+ *   GNU Lesser General Public License along with this file.
+ *   If not, see <https://www.gnu.org/licenses/>.
+ *
  */
+
+package tools.dynamia.modules.email.services;
 
 import org.springframework.transaction.annotation.Transactional;
 import tools.dynamia.modules.email.EmailMessage;
