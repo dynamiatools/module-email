c68deba316c0d63f7592a39dc2751a5f7eb2e037
Mario Serrano Leones <mario@dynamiasoluciones.com>
2023-12-09 10:01:47 -0500

    new email log and service improvements

--- sources/core/src/main/java/tools/dynamia/modules/email/domain/EmailMessageLog.java
+++ sources/core/src/main/java/tools/dynamia/modules/email/domain/EmailMessageLog.java
@@ -107,4 +107,9 @@ public class EmailMessageLog extends BaseEntitySaaS {
     public void setFrom(String from) {
         this.from = from;
     }
+
+    @Override
+    public String toString() {
+        return subject;
+    }
 }
